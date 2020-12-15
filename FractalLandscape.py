83e14dd90ad90d5048fde8981ee71de195ff87ec
austin-hill <41237424+austin-hill@users.noreply.github.com>
2020-12-15 14:17:46 +0000

    Update FractalLandscape.py

--- FractalLandscape.py
+++ FractalLandscape.py
@@ -1,54 +1,77 @@
+'''Uses the diamond-square algorithm to generate and plot a 3d fractal landscape'''
+
 import numpy as np
+from scipy.signal import medfilt2d
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def diamond_step(c1, c2, c3, c4, n):
-    return (c1+c2+c3+c4)/4 + np.random.randn()/(n+1)**2
+def diamond_step(corner_1, corner_2, corner_3, corner_4, n_iter):
+    return (corner_1+corner_2+corner_3+corner_4)/4 + np.random.randn()*2**(-n_iter)
 
-def square_step(v1, v2, v3, v4, n):
-    return (v1+v2+v3+v4)/4 + np.random.randn()/(n+1)**2
+def square_step(corner_1, corner_2, corner_3, corner_4, n_iter):
+    return (corner_1+corner_2+corner_3+corner_4)/4 + np.random.randn()*2**(-n_iter)
 
-def edge_square_step(v1, v2, v3, n):
-    return (v1+v2+v3)/4 + np.random.randn()/(n+1)**2
+def edge_square_step(corner_1, corner_2, corner_3, n_iter):
+    return (corner_1+corner_2+corner_3)/4 + np.random.randn()*2**(-n_iter)
 
 def diamond_square(fl, dim, k):
+
     for n_iterations in range(k):
-        
+
         # diamond step
         step = 2**(k-n_iterations)
         half_step = int(step/2)
         for i in range(0, dim-1, step):
             for j in range(0, dim-1, step):
-                fl[i+half_step, j+half_step] = diamond_step(fl[i, j], fl[i+step, j], fl[i, j+step], fl[i+step, j+step], n_iterations)
-        
+                fl[i+half_step, j+half_step] = diamond_step(fl[i, j], fl[i+step, j], fl[i, j+step],
+                                                            fl[i+step, j+step], n_iterations)
+
         # square step
         for i in range(half_step, dim, step):
             for j in range(0, dim, step):
                 if j == 0:
-                    fl[i, j] = edge_square_step(fl[i+half_step, j], fl[i-half_step, j], fl[i, j+half_step], n_iterations)
+                    fl[i, j] = edge_square_step(fl[i+half_step, j], fl[i-half_step, j],
+                                                fl[i, j+half_step], n_iterations)
                 elif j == dim-1:
-                    fl[i, j] = edge_square_step(fl[i+half_step, j], fl[i-half_step, j], fl[i, j-half_step], n_iterations)
+                    fl[i, j] = edge_square_step(fl[i+half_step, j], fl[i-half_step, j],
+                                                fl[i, j-half_step], n_iterations)
                 else:
-                    fl[i, j] = square_step(fl[i+half_step, j], fl[i-half_step, j], fl[i, j+half_step], fl[i, j-half_step], n_iterations)
+                    fl[i, j] = square_step(fl[i+half_step, j], fl[i-half_step, j],
+                                           fl[i, j+half_step], fl[i, j-half_step], n_iterations)
         for i in range(0, dim, step):
             for j in range(half_step, dim, step):
                 if i == 0:
-                    fl[i, j] = edge_square_step(fl[i+half_step, j], fl[i, j-half_step], fl[i, j+half_step], n_iterations)
+                    fl[i, j] = edge_square_step(fl[i+half_step, j], fl[i, j-half_step],
+                                                fl[i, j+half_step], n_iterations)
                 elif i == dim-1:
-                    fl[i, j] = edge_square_step(fl[i-half_step, j], fl[i, j+half_step], fl[i, j-half_step], n_iterations)
+                    fl[i, j] = edge_square_step(fl[i-half_step, j], fl[i, j+half_step],
+                                                fl[i, j-half_step], n_iterations)
                 else:
-                    fl[i, j] = square_step(fl[i+half_step, j], fl[i-half_step, j], fl[i, j+half_step], fl[i, j-half_step], n_iterations)
+                    fl[i, j] = square_step(fl[i+half_step, j], fl[i-half_step, j],
+                                           fl[i, j+half_step], fl[i, j-half_step], n_iterations)
+
     return fl
 
-power = 10
+# initialise variables
+power = 8
 size = 2**power+1
-fractal_landscape = diamond_square(np.zeros((size, size)), size, power)
 x = np.arange(size)
 x, y = np.meshgrid(x, x)
-        
-fig = plt.figure()
+# generate fractal - a median filter has been applied to remove occasional spikes which are
+# a known artifact of this algorithm
+fractal_landscape = medfilt2d(diamond_square(np.zeros((size, size)), size, power), kernel_size=3)
+
+# create figure
+fig = plt.figure(figsize=(15, 10))
 ax = Axes3D(fig)
-ax = fig.add_subplot(111, projection='3d')
+
+# adjust scaling of axes
+scale_x = 1
+scale_y = 1
+scale_z = 0.5
+ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([scale_x, scale_y, scale_z, 1]))
+
+# plot the result
 ax.plot_wireframe(x, y, fractal_landscape)
 
 plt.show()
